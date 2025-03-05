e8e79f66d0da5aae65671e4c7696186af03fddc9
shahin-ce <shahin.islam@cesystems.com>
2025-03-05 11:51:48 +1100

    del entry

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,10 @@ export default function App() {
       next: (data) => setTodos([...data.items]),
     });
   }
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   useEffect(() => {
     listTodos();
@@ -37,7 +41,8 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
